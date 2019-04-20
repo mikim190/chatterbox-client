d489cfd78b9aefc84d1c22dc6723e88c0e32437f
mikim190 <mykim.tran297@gmai.com>
2019-04-19 18:56:19 -0700

    Begin implementation on adding elements to the DOM

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,9 @@
 var MessageView = {
 
-  render: _.template(`
-      <!--
-      <div class="chat">
-        <div class="username"></div>
-        <div></div>
-      </div>
-      -->
-    `)
+  render: _.template(`<div class="chat">
+        <div class="username"><%= username %></div>
+        <div class="text"><%= text %></div>
+        <div class="roomname"><%= roomname %></div>
+      </div>`)
 
 };
--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -2,10 +2,18 @@ var MessagesView = {
 
   $chats: $('#chats'),
 
-  initialize: function() {
+  initialize: function () {
+    // App.fetch() gets all 100 messages
+    // Put all 100 message into render
   },
 
-  render: function() {
-  }
+  render: function () {
+    // render will divide the data into individual messages
+    // pass each individual message into renderMessage
+  },
 
+  renderMessage: function (message) {
+    // will put each message into $chats
+    MessagesView.$chats.append(MessageView.render(message));
+  }
 };
